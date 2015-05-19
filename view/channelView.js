e89b5d00b454046436d7683240f25eee5e6086f8
kingcole <sp@ki-i.at>
2015-05-19 14:01:12 +0200

    channels always in the background

--- view/channelView.js
+++ view/channelView.js
@@ -15,36 +15,8 @@ ACS.channelView = function(model, // ACS.model
 // ***********************************************************************************************************************
 	var returnObj = {};
 	
-	returnObj.line = new Kinetic.Line({
-		points: [0, 0, 0, 0],
-		stroke: ACS.vConst.CHANNELVIEW_STROKECOLOR,
-		strokeWidth: ACS.vConst.CHANNELVIEW_STROKEWIDTH,
-		dash: [5, 5],
-		dashEnabled: false,
-		// set custom hitRegion to be shorter than actual channel to avoid KinteicJs antiAliasing-bug
-		hitFunc: function(context) {
-			context.beginPath();
-			context.moveTo(returnObj.line.points()[0], returnObj.line.points()[1]);
-			var channelEnd = [];
-			if (returnObj.line.points()[2] > returnObj.line.points()[0])
-				channelEnd[0] = returnObj.line.points()[2]-2;
-			else
-				channelEnd[0] = returnObj.line.points()[2]+2;
-			if (returnObj.line.points()[3] > returnObj.line.points()[1])
-				channelEnd[1] = returnObj.line.points()[3]-2;
-			else
-				channelEnd[1] = returnObj.line.points()[3]+2;			
-			context.lineTo(channelEnd[0], channelEnd[1]);
-			this.setStrokeWidth(ACS.vConst.CHANNELVIEW_HITREGIONWIDTH);
-			context.fillStrokeShape(this);
-			this.setStrokeWidth(ACS.vConst.CHANNELVIEW_STROKEWIDTH);
-		}
-	});
+	returnObj.line = {};
 	
-	returnObj.line.on('mousedown', function(e) {
-		e.cancelBubble = true; // prevents modelView from starting a focusRect
-	});
-
 	returnObj.setStartPoint = function(x, y) {
 		if (returnObj.line) {
 			returnObj.line.points([x, y, returnObj.line.points()[2], returnObj.line.points()[3]]);
@@ -76,7 +48,38 @@ ACS.channelView = function(model, // ACS.model
 // ***********************************************************************************************************************
 // ************************************************** constructor code ***************************************************
 // ***********************************************************************************************************************
+	returnObj.line = new Kinetic.Line({
+		points: [0, 0, 0, 0],
+		stroke: ACS.vConst.CHANNELVIEW_STROKECOLOR,
+		strokeWidth: ACS.vConst.CHANNELVIEW_STROKEWIDTH,
+		dash: [5, 5],
+		dashEnabled: false,
+		// set custom hitRegion to be shorter than actual channel to avoid KinteicJs antiAliasing-bug
+		hitFunc: function(context) {
+			context.beginPath();
+			context.moveTo(returnObj.line.points()[0], returnObj.line.points()[1]);
+			var channelEnd = [];
+			if (returnObj.line.points()[2] > returnObj.line.points()[0])
+				channelEnd[0] = returnObj.line.points()[2]-2;
+			else
+				channelEnd[0] = returnObj.line.points()[2]+2;
+			if (returnObj.line.points()[3] > returnObj.line.points()[1])
+				channelEnd[1] = returnObj.line.points()[3]-2;
+			else
+				channelEnd[1] = returnObj.line.points()[3]+2;			
+			context.lineTo(channelEnd[0], channelEnd[1]);
+			this.setStrokeWidth(ACS.vConst.CHANNELVIEW_HITREGIONWIDTH);
+			context.fillStrokeShape(this);
+			this.setStrokeWidth(ACS.vConst.CHANNELVIEW_STROKEWIDTH);
+		}
+	});
+	
+	returnObj.line.on('mousedown', function(e) {
+		e.cancelBubble = true; // prevents modelView from starting a focusRect
+	});
+	
 	modelLayer.add(returnObj.line);
+	returnObj.line.moveToBottom();
 	
 	return returnObj;
 }
