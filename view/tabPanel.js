00219597f0206ff573abef737645c65a0c8ceacb
kingcole <sp@ki-i.at>
2015-06-03 14:13:24 +0200

    header added

--- view/tabPanel.js
+++ view/tabPanel.js
@@ -1,4 +1,32 @@
-ACS.tabPanelKeyCodes = {
+/*
+ * AsTeRICS - Assistive Technology Rapid Integration and Construction Set (http://www.asterics.org)
+ * 
+ * 
+ * Y88b                     d88P      888               d8888  .d8888b.   .d8888b. 
+ *  Y88b                   d88P       888              d88888 d88P  Y88b d88P  Y88b
+ *   Y88b                 d88P        888             d88P888 888    888 Y88b.
+ *    Y88b     d888b     d88P .d88b.  8888888b.      d88P 888 888         "Y888b.  
+ *     Y88b   d88888b   d88P d8P  Y8b 888   Y88b    d88P  888 888            "Y88b.
+ *      Y88b d88P Y88b d88P  88888888 888    888   d88P   888 888    888       "888
+ *       Y88888P   Y88888P   Y8b.     888   d88P  d8888888888 Y88b  d88P Y88b  d88P
+ *        Y888P     Y888P     "Y8888  8888888P"  d88P     888  "Y8888P"   "Y8888P"
+ * 
+ * Copyright 2015 Kompetenznetzwerk KI-I (http://ki-i.at)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+ ACS.tabPanelKeyCodes = {
   TAB: 9,
   ENTER: 13,
   ESC: 27,
@@ -17,12 +45,17 @@ ACS.tabPanelKeyCodes = {
 // - all tabs need to be of class "tab"
 // - all panels need to be of class "panel"
 ACS.tabPanel = function(id, tab, panel) { // String; the id of the div-container holding the tabPanel
-	// *************************************************************************************************************************
+
+// ***********************************************************************************************************************
+// ************************************************** private variables **************************************************
+// ***********************************************************************************************************************
 	var $panel = $('#' + id);  // the jQuery object for the panel
 	var $tabs = $panel.find('.' + tab); // Array of panel tabs.
 	var $panels = $panel.children('.' + panel); // Array of panels.			
-	
-	// private methods
+
+// ***********************************************************************************************************************
+// ************************************************** private methods ****************************************************
+// ***********************************************************************************************************************
 	var initPanel = function() {
 		var $tab; // the selected tab
 
@@ -344,7 +377,9 @@ ACS.tabPanel = function(id, tab, panel) { // String; the id of the div-container
 		return true;
 	}	
 	
-	// public stuff
+// ***********************************************************************************************************************
+// ************************************************** public stuff *******************************************************
+// ***********************************************************************************************************************
 	var returnObj = {};
 	
 	returnObj.updatePanel = function() {
@@ -357,7 +392,9 @@ ACS.tabPanel = function(id, tab, panel) { // String; the id of the div-container
 	
 	returnObj.events = ACS.eventManager();
 
-	// constructor code
+// ***********************************************************************************************************************
+// *********************************************** constructor code ***************************************************
+// ***********************************************************************************************************************
 	bindEventHandlers(); 
 	initPanel();
 	
