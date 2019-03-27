ebcf00e220edc65b46689d44992ee8e3c13b83df
Alija Sabic <sabic.alija@gmail.com>
2019-03-27 18:33:39 +0100

    release: correct zip path comment

--- src/scripts/semantic-release/prepare.js
+++ src/scripts/semantic-release/prepare.js
@@ -2,12 +2,12 @@ const { lstatSync, readdirSync, symlinkSync } = require("fs");
 
 let version = process.argv[2];
 
-/* Search `docs.zip` in root folder */
+/* Search `WebACS.zip` in root folder */
 const release = readdirSync(process.cwd())
   .filter(e => !lstatSync(e).isDirectory())
   .filter(e => e.match(/^WebACS.zip$/));
 
-/* Rename if `docs.zip` found */
+/* Rename if `WebACS.zip` found */
 if (release.length > 0) {
   symlinkSync("WebACS.zip", `WebACS-v${version}.zip`);
 }
