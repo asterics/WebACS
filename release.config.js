51dadb385cb127af5765e71f788f22776a547814
Alija Sabic <sabic.alija@gmail.com>
2019-03-29 01:23:11 +0100

    release: change debug output

--- release.config.js
+++ release.config.js
@@ -60,7 +60,8 @@ const config = {
   ci: false
 };
 
+console.log(release_comment);
 console.log(successComment);
-console.log(config);
+console.log(JSON.stringify(config,null,4));
 
 module.exports = config;
